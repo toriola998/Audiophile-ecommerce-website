28fde0e8ebcabfff949d1fa8ee58077c6c09c62f
Toriola Faidat <faaedah@gmail.com>
2022-02-16 21:09:18 +0100

    added styles to header info

--- src/App.vue
+++ src/App.vue
@@ -12,10 +12,16 @@ export default {
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Manrope&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Manrope:wght@400;500;700&display=swap');
     * {
         padding: 0;
         margin: 0;
         box-sizing: border-box;
+    }
+
+    body {
+        font-family: 'Manrope', sans-serif;
     }
 
     .flex{
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,13 @@
 <template>
     <Header/>
-    <div class="head">
-        <p>NEW PRODUCT</p>
-        <h1>XX99 Mark II HEADPHONES</h1>
-        <p>Experience natural, lifelike audio and exceptional build 
-        quality made for the passionate music enthusiast.</p>
-        <a href="">SEE PRODUCT</a>
+    <div class="head flex">
+        <div>
+            <p class="new">NEW PRODUCT</p>
+            <h1>XX99 MARK II HEADPHONES</h1>
+            <p class="note">Experience natural, lifelike audio and exceptional build 
+            quality made for the passionate music enthusiast.</p>
+            <a href="" class="cta">SEE PRODUCT</a>
+        </div> 
     </div>
 </template>
 
@@ -15,7 +17,6 @@ import Header from './../components/Header.vue'
 export default {
     name: 'Home',
     components: { Header,
-    
     }
 }
 </script>
@@ -28,5 +29,47 @@ export default {
         background-position: center; /* Center the image */
         background-repeat: no-repeat; /* Do not repeat the image */
         background-size: cover; 
+        justify-content: center;
+    }
+
+    .head > div {
+        width: 75%;
+    }
+
+    .new {
+        font-weight: 400;
+        letter-spacing: 6px; 
+        font-size: 0.9rem;
+        margin-bottom: 1rem;
+    }
+
+    .new,
+    .note {
+        opacity: .7;
+    }
+
+    p, h1, .cta{
+        text-align: center;
+        color: white;
+    }
+
+    h1 {
+        font-weight: 700;
+    }
+
+    .note {
+        line-height: 1.7;
+        margin: 1rem 0;
+        font-size: .9rem;
+    }
+
+    .cta {
+        display: block;
+        margin: 0 auto;
+        background-color: #D87D4A;
+        width: 10rem;
+        font-size: 1rem;
+        height: 3rem;
+        padding-top: .8rem;
     }
 </style>
