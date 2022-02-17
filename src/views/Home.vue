577908cef42fdc48c1ec895617bdac2fb360f9e8
Toriola Faidat <faaedah@gmail.com>
2022-02-17 21:44:55 +0100

    created footer component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -39,17 +39,20 @@
             <a class="plain-link">SEE PRODUCT</a>
         </div>
     </div>
+    <Footer />
 </template>
 
 <script>
 import Header from './../components/Header.vue'
 import Products from './../components/Products.vue'
+import Footer from './../components/Footer.vue'
 
 export default {
     name: 'Home',
     components: { 
         Header,
-        Products
+        Products,
+        Footer
     }
 }
 </script>
