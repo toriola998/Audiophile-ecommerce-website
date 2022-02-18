ec64ff83a9a7e821610b296dcedf4c10b984b943
Toriola Faidat <faaedah@gmail.com>
2022-02-18 11:18:49 +0100

    created headphones view

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,23 @@
 import {createRouter, createWebHistory} from 'vue-router'
-import Home from '@/views/Home.vue'
+
+import Home from './../views/Home.vue'
+import Headphones from './../views/Headphones.vue'
 
 const router = createRouter({
     history:  createWebHistory(),
-    routes: [{
+    routes: [
+    {
         path: '/',
         name: 'Home',
         component: Home
-     }]
+    },
+
+    {
+        path: '/Headphones',
+        name: 'Headphones',
+        component: Headphones
+     }
+    ]
 })
 export default router;
 
---
+++ src/views/Headphones.vue
@@ -0,0 +1,13 @@
+<template>
+    <h2>Headphones</h2>
+</template>
+
+<script>
+export default {
+    name: 'Headphones'
+}
+</script>
+
+<style>
+
+</style>
