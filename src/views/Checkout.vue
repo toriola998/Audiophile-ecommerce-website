a1afea9fb785eec3f18aa625d6c1980b7ed1efc3
Toriola Faidat <faaedah@gmail.com>
2022-03-24 11:46:33 +0100

    added billing details to form

--- src/views/Checkout.vue
+++ src/views/Checkout.vue
@@ -6,6 +6,26 @@
                 <h1>CHECKOUT</h1>
             </header>
             <main>
+                <p class="orange">BILLING DETAILS</p>
+                <form> 
+                    <div class="input-wrap">
+                        <label for="Name">Name</label><br>
+                        <input type="text" placeholder="Alexei Ward" v-model="name"/>
+                        <!---<p class="error" v-if="nameError">Can't be empty</p>-->
+                    </div>
+
+                    <div class="input-wrap">
+                        <label for="Email">Email</label><br>
+                        <input type="email" placeholder="alexei@gmail.com" v-model="email"/>
+                        <!---<p class="error" v-if="nameError">Can't be empty</p>-->
+                    </div>
+
+                    <div class="input-wrap">
+                        <label for="Number">Number</label><br>
+                        <input type="number" placeholder="+1 202-555-0136" v-model="email"/>
+                        <!---<p class="error" v-if="nameError">Can't be empty</p>-->
+                    </div>
+                </form>
             </main>
         </div>
     </div>
@@ -26,12 +46,37 @@ export default {
 
 <style scoped>
     .checkout-container {
-        padding: 0 1.5rem;
+        padding: 2rem 1.5rem;
         background-color: #f1f1f1;
     }
 
     .checkout-container > div {
         background-color: #fff;
-        padding: 0 1rem;
+        padding: 2rem 1rem;
+        border-radius: 7px;
+    }
+
+    label, 
+    .orange {
+        font-weight: 700;
+    }
+
+    .orange {
+        color: #D87D4A;
+        font-size: .9rem;
+        margin: 1.5rem 0;
+    }
+
+    label {
+        font-size: .85rem;
+    }
+
+    input {
+        padding-left: 2rem;
+        border: 1px solid #cfcfcf;
+        border-radius: 5px;
+        width: 100%;
+        height: 3rem;
+        margin: .5rem 0 1.5rem;
     }
 </style>
