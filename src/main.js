ded7013b18bd14703846781ef6142e897011c278
Toriola Faidat <faaedah@gmail.com>
2022-04-01 19:05:13 +0100

    added vuex to project

--- src/main.js
+++ src/main.js
@@ -3,4 +3,5 @@ import App from './App.vue'
 import router from './router'
 
 //createApp(App).mount('#app')
-createApp(App).use(router).mount('#app')
+//createApp(App).use(router).mount('#app')
+createApp(App).use(store).use(router).mount('#app')
--- src/views/ProductDetail.vue
+++ src/views/ProductDetail.vue
@@ -115,7 +115,7 @@ export default {
         }
     },
     mounted() {
-        this.productDetails()
+       // this.productDetails()
     }
 }
 </script>
