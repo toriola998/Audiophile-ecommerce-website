61a4760042fcaecf24c34cc3c246253bb9753e6f
Toriola Faidat <faaedah@gmail.com>
2022-04-19 15:14:40 +0100

    added some specificity to product-layout.css

--- src/views/Speakers.vue
+++ src/views/Speakers.vue
@@ -61,7 +61,6 @@ export default {
 }
 </script>
 
-
 <!--ORDINARY 'import styles' WOULDN'T WORK, IT WASN'T SCOPED,
  KEEPS MESSING WITH OTHER ELEMENTS----------->
 <style src="./../assets/product-layout.css" scoped>   
